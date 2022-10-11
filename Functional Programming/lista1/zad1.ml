c68bdfa30ba6d0f6bb54a2b752d3136317229d6e
Michal <michalfica125@gmail.com>
2022-10-11 19:17:57 +0200

    first task

--- Functional Programming/lista1/zad1.ml
+++ Functional Programming/lista1/zad1.ml
@@ -1,5 +1,14 @@
 fun x -> x;;
 (* - : 'a -> 'a = <fun> *)
 
-fun x -> if x + x > x then x else x;;
-(* - : int -> int = <fun> *)
+(* TYPE int -> int = <fun> *)
+(* E.g: *) fun x -> x + 1;;
+
+(* TYPE (’a -> ’b) -> (’c -> ’a) -> ’c -> ’b *)
+(* E.g *) fun f -> fun g -> fun x -> (f)  ((g) x);;
+
+(* TYPE ’a -> ’b -> ’a *)
+(* E.g *) fun x -> fun y -> x;;
+
+(* TYPE ’a -> ’a -> ’a *)
+(* E.g *) fun y -> fun x -> if x>y then x else y;;
