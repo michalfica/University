2e404031cf7ea1935db06b69fddda0f47e13580b
Michal <michalfica125@gmail.com>
2022-11-01 08:59:50 +0100

    permutation module

--- Functional Programming/lista3/perm.ml
+++ Functional Programming/lista3/perm.ml
@@ -20,41 +20,45 @@ end
 module Make(Key : OrderedType) = 
 struct 
   
-module Aux = Map.Make(Key)
-  
-let id = (Aux.empty, Aux.empty)
+  module Aux = Map.Make(Key)
 
-let apply perm k = 
-  let perm, inver = perm in    
-    match Aux.find k perm with 
-           | exception Not_found -> k 
-           | value               -> value;;
+  type t = |Permutacja of (Key.t Aux.t) *  (Key.t Aux.t) 
 
-let invert perm = let perm, inver = perm in (inver,perm);;
+  let (id:t) = Permutacja (Aux.empty, Aux.empty)
 
-let swap key1 key2 = 
-  (Aux.add key1 key2 (Aux.add key2 key1 Aux.empty), 
-    Aux.add key2 key1 (Aux.add key1 key2 Aux.empty));;
-
-let compose p1 p2 = 
-  let p1, inv1 = p1 in 
-  let p2, inv2 = p2 in
-
-  let apply2 perm k =     
-    match Aux.find k perm with 
+  let apply perm k = 
+    let Permutacja (perm, inver) = perm in    
+      match Aux.find k perm with 
             | exception Not_found -> k 
-            | value               -> value in
+            | value               -> value;;
 
-  let merge p1 p2 = 
-    let singleKeyCompose k x y = 
-      match x with 
-      | None   -> Some (apply2 p2 k) 
-      | Some x -> Some (apply2 p2 x)
-    in Aux.merge singleKeyCompose p1 p2 in 
+  let invert perm = 
+    let Permutacja (perm, inver) = perm in 
+    Permutacja (inver,perm);;
 
-  (merge p1 p2, merge inv2 inv1)
+  let swap key1 key2 = 
+    Permutacja (Aux.add key1 key2 (Aux.add key2 key1 Aux.empty), 
+      Aux.add key2 key1 (Aux.add key1 key2 Aux.empty));;
 
-let compare p1 p2 = 
-  Aux.compare Key.compare p1 p2 
+  let compose p1 p2 = 
+    let Permutacja (p1, inv1) = p1 in 
+    let Permutacja (p2, inv2) = p2 in
+
+    let apply2 p k =     
+      match Aux.find k p with 
+              | exception Not_found -> k 
+              | value               -> value in
+
+    let merge p1 p2 = 
+      let singleKeyCompose k x y = 
+        match x with 
+        | None   -> Some (apply2 p2 k) 
+        | Some x -> Some (apply2 p2 x)
+      in Aux.merge singleKeyCompose p1 p2 in 
+
+    Permutacja (merge p1 p2, merge inv2 inv1)
+
+  let compare p1 p2 = 
+    Aux.compare Key.compare p1 p2 
 
 end 
